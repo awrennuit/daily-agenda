09815e0c9a76e50147d191f8976933f13a23b604
AwrenNuit <a.rennaker@yahoo.com>
2020-02-29 16:18:02 -0600

    updated title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Daily Agenda</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
