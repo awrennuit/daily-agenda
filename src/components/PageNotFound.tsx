3a6a0c40c0b2b485b8c73b70aaca3989d593bb41
AwrenNuit <a.rennaker@yahoo.com>
2020-02-29 20:26:27 -0600

    created files for bad url page

---
+++ src/components/PageNotFound.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PageNotFound.css';
+
+const PageNotFound: React.FC = () => {
+
+  return (
+    <div className="container">
+      <div className="list-container">
+        
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,17 @@
+import { IonContent, IonPage } from '@ionic/react';
+import React from 'react';
+import './NotFound.css';
+import PageNotFound from '../components/PageNotFound';
+
+const NotFound: React.FC = () => {
+
+  return (
+    <IonPage>
+      <IonContent>
+        <PageNotFound />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default NotFound;
