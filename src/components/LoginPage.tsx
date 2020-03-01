0ba5167f24d14823cca7dab6dfbc108a26af04aa
AwrenNuit <a.rennaker@yahoo.com>
2020-03-01 14:04:51 -0600

    added basic styling and formatting

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { IonCard, IonCardHeader, IonInput, IonButton } from '@ionic/react';
+import { IonCard, IonCardHeader, IonButton, IonLabel, IonInput, IonItem } from '@ionic/react';
+import './LoginPage.css';
 
 const LoginPage: React.FC = () => {
 
   return(
-    <div className="container">
+    <div className="login-container">
       <IonCard>
-        <IonCardHeader>Daily Agenda</IonCardHeader>
-        <IonInput></IonInput>
-        <IonInput></IonInput>
-        <IonButton>Login</IonButton>
-        <IonButton>register</IonButton>
+        <div className="login-card-container">
+          <IonCardHeader style={{fontSize:"1.5em",color:"#FECC27"}}>Log In</IonCardHeader>
+          <IonItem>
+            <IonLabel position="floating" color="secondary">Username </IonLabel>
+            <IonInput value="username"></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating" color="secondary">Password </IonLabel>
+            <IonInput type="password" value="password"></IonInput>
+          </IonItem>
+          <IonButton style={{margin:"30px"}}>Login</IonButton>
+          <div>
+            <IonButton color="medium" style={{marginBottom:"10px"}}>register</IonButton>
+          </div>
+        </div>
       </IonCard>
     </div>
   );
