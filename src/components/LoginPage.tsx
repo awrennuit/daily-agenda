aa574db5e6f733619b1388b6c8acfa5ff6c14fec
AwrenNuit <a.rennaker@yahoo.com>
2020-03-01 13:03:56 -0600

    basic template

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { IonCard, IonCardHeader, IonInput, IonButton } from '@ionic/react';
 
 const LoginPage: React.FC = () => {
 
   return(
-    <>
-    </>
+    <div className="container">
+      <IonCard>
+        <IonCardHeader>Daily Agenda</IonCardHeader>
+        <IonInput></IonInput>
+        <IonInput></IonInput>
+        <IonButton>Login</IonButton>
+        <IonButton>register</IonButton>
+      </IonCard>
+    </div>
   );
 };
 
